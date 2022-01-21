4e00b4e77bf573ff0de8a5951a27d86f9be0df6e
Abel Murgas <abel.murgas@mibus.com.pa>
2022-01-20 21:28:01 -0500

    add more feature

--- source/math_x/function_reading/funtion_reading.py
+++ source/math_x/function_reading/funtion_reading.py
@@ -1,4 +1,5 @@
 class AlgebraicFunctionReading:
+
     """
         This class is for working with algebraic function, validate and break down the attributes of the function
     """
@@ -48,31 +49,44 @@ class AlgebraicFunctionReading:
             self.list_function = list(args)
         else:
             if not len(kwargs) == 1:
-                raise Exception(self.__sed_errors(
+                raise Exception(self.__send_errors(
                     2, f"and You put\033[1m {len(kwargs)}:{list(kwargs.keys())}"))
             else:
                 if not list(kwargs.keys())[0] in kwargs_allows:
-                    raise Exception(self.__sed_errors(
+                    raise Exception(self.__send_errors(
                         1, f"And you use\033[1m {list(kwargs.keys())[0]}"))
                 index = kwargs_allows.index(list(kwargs.keys())[0])
-                if index == 2:
-                    if not type(list(kwargs.values())[0]) == list:
-                        raise Exception(self.__sed_errors(
-                            5, f", Expected \033[1m <class 'list'> \033[0;91m Obtained \033[1m {type(list(kwargs.values())[0])}"))
-                    self.list_function = list(kwargs)
-                elif index == 1:
-                    if not type(list(kwargs.values())[0]) == dict:
-                        raise Exception(self.__sed_errors(
-                            5, f", Expected \033[1m <class 'dict'> \033[0;91m Obtained \033[1m {type(list(kwargs.values())[0])}"))
-                    self.dict_function = kwargs[list(kwargs.keys())[0]]
 
-                else:
+                if index == 2:  # list validation
+                    if not type(list(kwargs.values())[0]) == list:
+                        raise Exception(self.__send_errors(
+                            5, f", Expected \033[1m <class 'list'> \033[0;91m Obtained \033[1m {type(list(kwargs.values())[0])}"))
+                    if not all(isinstance(n, (int, float)) for n in list(kwargs.values())[0]):
+                        raise Exception(self.__send_errors(
+                            3, f"Your arguments:\033[1m {args}"))
+                    self.grade = len(list(kwargs.values())[0])
+                    if not len(list(kwargs.values())[0]) > 1 and len(list(kwargs.values())[0]) < 5:
+                        raise Exception(self.__send_errors(
+                            6, f"The grade of the function received: \033[1m {self.grade}"))
+                    self.list_function = list(list(kwargs.values())[0])
+
+                elif index == 1:  # dict
+                    if not type(list(kwargs.values())[0]) == dict:
+                        raise Exception(self.__send_errors(
+                            5, f", Expected \033[1m <class 'dict'> \033[0;91m Obtained \033[1m {type(list(kwargs.values())[0])}"))
+                    if not list(kwargs.values())[0]:
+                        raise Exception(self.__send_errors(
+                            7, f"Your argument: \033[1m {kwargs}"))
+                    self.__validate_dict_function(list(kwargs.values())[0])
+                    self.dict_function = list(kwargs)[0]
+
+                else:  # str
                     if not type(list(kwargs.values())[0]) == str:
-                        raise Exception(self.__sed_errors(
+                        raise Exception(self.__send_errors(
                             5, f", Expected \033[1m <class 'str'> \033[0;91m Obtained \033[1m {type(list(kwargs.values())[0])}"))
                     self.str_function = kwargs[list(kwargs.keys())[0]]
 
-    def __sed_errors(self, n_error, extra_message=""):
+    def __send_errors(self, n_error, extra_message=""):
         """
         This function is to send different error, in the process. \n
         Errors:\n
@@ -80,7 +94,10 @@ class AlgebraicFunctionReading:
         2 - Only Allowed one representacion of a funtion\n
         3 - Only Allowed numbers (int and float) as arg\n
         4 - Only can use args o kwargs(to access the others method of representation of a function) , but can't use both at same time\n
-        5 - The type of the data is not what was expected 
+        5 - The type of the data is not what was expected\n
+        6 - The range of the grade allow is 1 to 5\n
+        7 - The name of variable need be unique\n
+        8 - Can use all abecedary letter as variable except c or k (constant variable)\n
         Args:
             name_e (int): Number of error
             extra_message (str): Extra message with the error.
@@ -90,19 +107,41 @@ class AlgebraicFunctionReading:
             2: "Only Allowed 1 representacion of a function ",
             3: "Only Allowed numbers (int and float) as arg ",
             4: "Only can use args o kwargs(to access the others method of representation of a function) , but can't use both at same time ",
-            5: "The type of the data is not what was expected "
+            5: "The type of the data is not what was expected ",
+            6: "The range of the grade allow is 1 to 5 ",
+            7: "Not empty values allow ",
+            8: "Can use all abecedary letter as variable except c or k (constant variable) "
         }
         if error.get(n_error):
             return '\033[91m' + error.get(n_error) + extra_message + '\033[0m'
         else:
             return print("Error not found")
 
-    def __validate_dict_functio():
-        pass
-    def __validate_list_functio():
-        pass
-    def __validate_str_functio():
-        pass
+    def __validate_dict_function(self, dict: dict):
+        """
+        This funtion validate if the dictionary function pass is correct and complies with the rules
+        1. The constant pass have key name = c or k
+        2. Can use all abecedary letter as variable example:
+        x2 = 2, z = -1 , c = 1 (2x**2-1z+1)
 
+        Args:
+            dict (dict): dictionary that will test
+        """
+        letter_variable = ['a', 'b', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'l', 'm',
+                           'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'q', 'w', 'x', 'y', 'z']
+        list_key = list(dict.keys())
+        try:
+            list_key_first = [i[0].lower() for i in list_key]
+        except:
+            raise Exception(self.__send_errors(
+                8, f"Your variable \033[1m {list_key}"))
 
-new = AlgebraicFunctionReading(str_function="2")
+        have_allow_variable = all(
+            letter in letter_variable for letter in list_key_first)
+
+        if not have_allow_variable:
+            raise Exception(self.__send_errors(
+                8, f"Your variable name \033[1m {list_key}"))
+
+    def __validate_str_function():
+        pass
